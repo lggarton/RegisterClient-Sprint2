71eec794f611d1c51bd653f338d4f78eee48bdb3
Abby Oberle <aoberle@uark.edu>
2018-04-17 12:49:42 -0500

    3.6 updates.. spent about 2 hours trying to fix my errors

--- app/src/main/java/edu/uark/uarkregisterapp/ReportActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/ReportActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import java.util.List;
+import android.widget.ListView;
 
+import edu.uark.uarkregisterapp.models.transition.TransactionEntryTransition;
 import edu.uark.uarkregisterapp.models.transition.TransactionTransition;
 
 public class ReportActivity extends AppCompatActivity {
     private Button returnButton;
     private TransactionTransition transactionTransition;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,12 @@ public class ReportActivity extends AppCompatActivity {
 
         // Pull the Transaction and TransactionEntries in from the previous activity
         this.transactionTransition = this.getIntent().getParcelableExtra(this.getString(R.string.intent_extra_transaction));
+        if (this.getIntent() != null) {
+            list = findViewById(R.id.list_view_products);
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, getEntries());
+            list.setAdapter(adapter);
+            // does not print out the list -- still working on this
+        }
 
         returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
@@ -28,8 +39,17 @@ public class ReportActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+    }
 
-        //ScrollView scroll = (ScrollView)findViewById(R.id.scroll);
+    private double getTotal() {
+        return this.transactionTransition.getTotalAmount();
+    }
 
+    private List<TransactionEntryTransition> getEntries() {
+        return this.transactionTransition.getEntryTransitions();
+    }
+
+    private ListView getTransactions() {
+        return (ListView) this.findViewById(R.id.list_view_products);
     }
 }
