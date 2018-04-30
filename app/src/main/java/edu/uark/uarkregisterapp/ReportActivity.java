aab69779f4bde1528af8acab5e1bf68f1bea4004
Abby Oberle <aoberle@uark.edu>
2018-04-30 17:28:12 -0500

    3.6 updates

--- app/src/main/java/edu/uark/uarkregisterapp/ReportActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/ReportActivity.java
@@ -1,23 +1,18 @@
 package edu.uark.uarkregisterapp;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import java.util.ArrayList;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 import edu.uark.uarkregisterapp.adapters.TransactionReportAdapter;
-import edu.uark.uarkregisterapp.models.api.ApiResponse;
-import edu.uark.uarkregisterapp.models.api.Transaction;
-import edu.uark.uarkregisterapp.models.api.services.TransactionService;
 import edu.uark.uarkregisterapp.models.transition.TransactionEntryTransition;
 import edu.uark.uarkregisterapp.models.transition.TransactionTransition;
+import edu.uark.uarkregisterapp.models.api.Transaction;
 
 
 public class ReportActivity extends AppCompatActivity {
@@ -39,6 +34,7 @@ public class ReportActivity extends AppCompatActivity {
         this.reportAdapter = new TransactionReportAdapter(this, this.arrayList);
         this.getProductsListView().setAdapter(this.reportAdapter);
 
+        this.transaction = new Transaction(this.transactionTransition);
         TextView totalAmountText = (TextView) this.findViewById(R.id.total);
         totalAmountText.setText(Double.toString(transaction.getTotalAmount()));
 
@@ -54,38 +50,5 @@ public class ReportActivity extends AppCompatActivity {
 
     private ListView getProductsListView() {
         return (ListView) this.findViewById(R.id.list_view_products);
-    }
-
-    private TextView getTotalListed() {
-        return (TextView) this.findViewById(R.id.total);
-    }
-
-    private class CreateTransactionTask extends AsyncTask<Transaction, Void, ApiResponse<Transaction>> {
-
-        @Override
-        protected ApiResponse<Transaction> doInBackground(Transaction... transactions) {
-            if (transactions.length > 0) {
-                return (new TransactionService()).createTransaction(transactions[0]);
-            } else {
-                return (new ApiResponse<Transaction>())
-                        .setValidResponse(false);
-            }
-        }
-
-        @Override
-        protected void onPostExecute(ApiResponse<Transaction> apiResponse) {
-//            this.createEmployeeAlert.dismiss();
-
-//            if (!apiResponse.isValidResponse()) {
-//                new AlertDialog.Builder(CreateEmployeeActivity.this)
-//                        .setMessage(R.string.alert_dialog_employee_create_failed)
-//                        .create()
-//                        .show();
-//                return;
-//            }
-
-        }
-
-        private AlertDialog createEmployeeAlert;
     }
 }
